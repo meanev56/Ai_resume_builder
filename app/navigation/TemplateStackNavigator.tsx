a7dcae76bb3ecf9142e4cc62a04f8f1cc1ed73b1
meanev56 <emekanjoku86@gmail.com>
2025-11-15 13:15:50 +0100

    update templatestacknavigator

--- app/navigation/TemplateStackNavigator.tsx
+++ app/navigation/TemplateStackNavigator.tsx
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TemplatePreviewScreen from '../screens/TemplatePreviewScreen';
+import TemplateScreen from '../screens/TemplateScreen';
 
-export class TemplateStackNavigator extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
-  }
+
+const Stack = createNativeStackNavigator();
+
+const TemplateStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name='Templates'
+        component={TemplateScreen}
+        options={{ headerShown: false}}
+      />
+      <Stack.Screen 
+        name='TemplatePreview'
+        component={TemplatePreviewScreen}
+        options={{ headerShown: false}}
+      />
+      
+    </Stack.Navigator>
+  )
 }
 
 export default TemplateStackNavigator
